3667b87f1ad6c0c5471e7dea2254716a034443b1
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-02 20:36:39 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  max = 999
+  pal = []
+  trip_digs = (100..999).to_a.reverse
+  
+  trip_digs.each do |num|
+    max.downto(100).each do |num2|
+      n = num * num2
+      if is_palindrome?(n)
+        pal.empty? ? pal << n : n > pal[0] ? pal[0] = n : break
+      end
+    end
+    max -= 1
+  end
+  
+  pal[0]
+end
+
+
+def is_palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+  def answer
+    max = 999
+    pal = []
+    trip_digs = (100..999).to_a.reverse
+    
+    trip_digs.each do |num|
+      max.downto(100).each do |num2|
+        @n = num * num2
+        if palindrome?
+          pal.empty? ? pal << @n : @n > pal[0] ? pal[0] = @n : break
+        end
+      end
+      max -= 1
+    end
+    
+    pal[0]
+  end
+
+  def palindrome?
+    @n.to_s == @n.to_s.reverse
+  end
+end
